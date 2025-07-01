b5828640d28efd133c6f85bd84ef0dde8e5f04bb
Taylor Hickman <taylor.hickman@proton.me>
2025-07-01 17:19:15 -0500

    Update menu items with new products and pricing
    - Classic Lemonade: .00
    - Fresh Cucumber: .00
    - Fresh Herb Bag: .00

--- app/utils/menuItems.ts
+++ app/utils/menuItems.ts
@@ -2,8 +2,8 @@ import { MenuItem } from '../types'
 
 export async function getMenuItems(): Promise<MenuItem[]> {
   return [
-    { name: 'Classic Lemonade', price: 1 },
-    { name: 'Item 2', price: 1.25 },
-    { name: 'Item 3', price: 0.75 }
+    { name: 'Classic Lemonade', price: 2 },
+    { name: 'Fresh Cucumber', price: 1 },
+    { name: 'Fresh Herb Bag', price: 3 }
   ]
 }
